202450649cd25af9f3f1ec78e855c497f676066b
ducbe <ducbe2k2@gmail.com>
2021-12-11 08:25:34 +0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
-import '../styles/globals.css'
 import Layout from "../components/Layout";
 import 'react-loading-skeleton/dist/skeleton.css'
-
+import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   return (
       <Layout>
