136cd32cd8c395cd32b17259da6965866124efb3
ducbe <ducbe2k2@gmail.com>
2021-12-11 22:52:44 +0700

    first commit

--- pages/pokemon.js
+++ pages/pokemon.js
@@ -9,6 +9,8 @@ import Zoom from 'react-medium-image-zoom'
 
 export default function pokemon(pokemon){
     // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [showModal, setShowModal] =useState(false);
+    // eslint-disable-next-line react-hooks/rules-of-hooks
     const [loading,setLoad] = useState(true)
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const [allPokemon,setAllPokemon] = useState([]);
@@ -141,8 +143,8 @@ export default function pokemon(pokemon){
                             </a>
                             <div className="flex flex-col flex-1 p-6">
                                 <a href="#" aria-label="Te nulla oportere reprimique his dolorum"></a>
-                                <a href="#" className="text-xs tracking-wider uppercase hover:underline text-violet-600">Type: {pokemon.types[0].type.name}</a>
-                                <a href="#" className="flex-1 py-2 text-lg font-semibold leading-snug">{pokemon.name}</a>
+                                <a onClick={() => setShowModal(true)} href="#" className="text-xs tracking-wider uppercase hover:underline text-violet-600">Type: {pokemon.types[0].type.name}</a>
+                                <a onClick={() => setShowModal(true)} href="#" className="flex-1 py-2 text-lg font-semibold leading-snug">{pokemon.name}</a>
 
                             </div>
                         </article>
@@ -174,6 +176,50 @@ export default function pokemon(pokemon){
                 </div>
 
             </section>
+            {showModal ? (
+                <>
+                    <div
+                        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                    >
+                        <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                            {/*content*/}
+                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                {/*header*/}
+                                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                                    <h3 className="text-3xl font-semibold text-center">
+                                        Hi!
+                                    </h3>
+                                    <button
+                                        className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                        onClick={() => setShowModal(false)}
+                                    >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                                    </button>
+                                </div>
+                                {/*body*/}
+                                <div className="relative p-6 flex-auto">
+                                    <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
+                                        Chức Năng Đang Được Phát Triển
+                                    </p>
+                                </div>
+                                {/*footer*/}
+                                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                                    <button
+                                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        type="button"
+                                        onClick={() => setShowModal(false)}
+                                    >
+                                        Close
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                </>
+            ) : null}
         </>
     )
 }
