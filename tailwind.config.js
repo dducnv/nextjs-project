c3ab6069b74b989257ad8ec8249aca52bdab7354
ducbe <ducbe2k2@gmail.com>
2021-12-11 08:20:39 +0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 module.exports = {
-    purge: [ "./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", ],
-    darkMode: false, // or 'media' or 'class'
+    future: {
+        removeDeprecatedGapUtilities: true,
+    },
+    purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'accent-1': '#333',
+            },
+        },
     },
-    variants: {
-        extend: {},
-    },
+    variants: {},
     plugins: [],
 }
