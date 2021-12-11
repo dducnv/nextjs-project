e8c9b749f03159c2113d020f0957ada6e68bd7aa
ducbe <ducbe2k2@gmail.com>
2021-12-11 08:15:54 +0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import Layout from "../components/Layout";
+import 'tailwindcss/tailwind.css'
 import 'react-loading-skeleton/dist/skeleton.css'
 
 function MyApp({ Component, pageProps }) {
