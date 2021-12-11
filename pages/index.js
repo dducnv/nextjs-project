e07e0677e3e7f64eb1efefc03d7b527ba9175842
ducbe <ducbe2k2@gmail.com>
2021-12-11 23:00:05 +0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,12 @@ export default function Home() {
     <>
       <Head>
         <title>Home | 7th.Dec</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta charSet="UTF-8"/>
+         <meta name="description" content="Này Là Web Của Đức"/>
+          <meta itemProp="image" content="https://uploads-ssl.webflow.com/612a3d9cd9c35344a2460d68/61b4ba4c8e9e68583ebecde4_127276256_1330240820660968_9032823244503499320_n.jpg"/>
+          <meta name="keywords" content="HTML, CSS, JavaScript, reacjs, nextjs"/>
+         <meta name="author" content="Nguyễn Văn Đức"/>
+         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <link rel="icon" type="image/png" href="https://uploads-ssl.webflow.com/612a3d9cd9c35344a2460d68/61b2181355e88e77082f397d_Myloc.svg"/>
         <link rel="icon" type="image/png" href="https://uploads-ssl.webflow.com/612a3d9cd9c35344a2460d68/61b2181355e88e77082f397d_Myloc.svg"/>
       </Head>
